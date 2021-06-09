05c469269f84f87be56613c1fa1dfd3546548c35
Cookieman01 <13785517+Cookieman01@users.noreply.github.com>
2021-06-09 10:55:48 +0200

    Added Death

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ clock = pygame.time.Clock()
 # Create screen object
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+# Custom event, every second, to update infection
+INFECTEVENT = pygame.USEREVENT+1
+pygame.time.set_timer(INFECTEVENT, 1000)
+
 # create group of human
 all_human = pygame.sprite.Group()
 for i in range(AMOUNT_HUMANS):
-    if i == 1:
-        human = Human(is_infected=True)
+    if i == 0:
+        human = Human(status=1)
     else:
         human = Human()
     all_human.add(human)
@@ -34,6 +38,9 @@ while running:
             # Check if it was ESC
             if event.key == K_ESCAPE:
                 running = False
+        if event.type == INFECTEVENT:
+            for human in all_human:
+                human.infect_update()
 
         # Check if user clicked close
         if event.type == pygame.QUIT:
@@ -43,6 +50,7 @@ while running:
     for human in all_human:
         human.move()
         human.check_collision(all_human)
+
 
     # --------DISPLAY----------------------------------
     # Fill Screen Black
--- person.py
+++ person.py
@@ -6,45 +6,53 @@ from config import *
 
 
 class Human(pygame.sprite.Sprite):
-    def __init__(self, is_infected=False):
+    def __init__(self, status=0):
         super(Human, self).__init__()
         self.width, self.height = 5, 5
         self.surf = pygame.Surface((self.width, self.height))
         self.vel_x = 0
         self.vel_y = 0
         self.radius = self.width / 2
-        self.is_infected = is_infected
+        self.status = status
+        # 0 = Not Infected
+        # 1 = Infected
+        # 2 = Quarantined
+        # 3 = Healed
+        # 4 = Dead
+        self.heal_prob = 0
+        self.death_prob = 0
 
         self.surf.set_colorkey(BLACK)
         self.rect = self.surf.get_rect()
         self.rect.move_ip(random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT))
         self.surf.fill(BLACK)
         pygame.draw.circle(self.surf, WHITE, (self.width / 2, self.height / 2), self.radius)
-        if self.is_infected:
+        if self.status == 1:
             self.infect()
 
     def move(self):
-        self.vel_x = self.vel_x + random.randint(-SPEED, SPEED)
-        self.vel_y = self.vel_y + random.randint(-SPEED, SPEED)
-        if self.vel_x > MAX_SPEED:
-            self.vel_x = MAX_SPEED
-        elif self.vel_x < -MAX_SPEED:
-            self.vel_x = -MAX_SPEED
-        if self.vel_y > MAX_SPEED:
-            self.vel_y = MAX_SPEED
-        elif self.vel_y < -MAX_SPEED:
-            self.vel_y = -MAX_SPEED
-        self.rect.move_ip(self.vel_x, self.vel_y)
+        if self.status != 4:
+            self.vel_x = self.vel_x + random.randint(-SPEED, SPEED)
+            self.vel_y = self.vel_y + random.randint(-SPEED, SPEED)
+            if self.vel_x > MAX_SPEED:
+                self.vel_x = MAX_SPEED
+            elif self.vel_x < -MAX_SPEED:
+                self.vel_x = -MAX_SPEED
+            if self.vel_y > MAX_SPEED:
+                self.vel_y = MAX_SPEED
+            elif self.vel_y < -MAX_SPEED:
+                self.vel_y = -MAX_SPEED
+            self.rect.move_ip(self.vel_x, self.vel_y)
 
-        # Teleport to other side with rect is crossing border
-        if self.rect.left < 0:
-            self.rect.left = SCREEN_WIDTH - self.width
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = 0 + self.width
-        if self.rect.top < 0:
-            self.rect.top = SCREEN_HEIGHT - self.height
-        if self.rect.bottom > SCREEN_HEIGHT:
-            self.rect.bottom = 0 + self.height
+            # Teleport to other side with rect is crossing border
+            if self.rect.left < 0:
+                self.rect.left = SCREEN_WIDTH - self.width
+            if self.rect.right > SCREEN_WIDTH:
+                self.rect.right = 0 + self.width
+            if self.rect.top < 0:
+                self.rect.top = SCREEN_HEIGHT - self.height
+            if self.rect.bottom > SCREEN_HEIGHT:
+                self.rect.bottom = 0 + self.height
 
     def check_collision(self, group):
         hits = pygame.sprite.spritecollide(self, group, False)
@@ -53,9 +61,22 @@ class Human(pygame.sprite.Sprite):
             self.vel_y = 0
 
             for human in hits:
-                if human.is_infected:
+                if human.status == 1 and not self.status == 3 and not self.status == 4:
                     self.infect()
 
     def infect(self):
-        self.is_infected = True
+        self.status = 1
         pygame.draw.circle(self.surf, RED, (self.width / 2, self.height / 2), self.radius)
+
+    def infect_update(self):
+        if self.status == 1:
+            self.heal_prob += HEALING_PROB_INCREASE
+            if self.death_prob < MAX_DEATH_PROB:
+                self.death_prob += DEATH_PROB_INCREASE
+            dice = random.randint(0, 100)
+            if dice >= 100 - self.death_prob:
+                self.status = 4
+                pygame.draw.circle(self.surf, GRAY, (self.width / 2, self.height / 2), self.radius)
+            if dice <= self.heal_prob:
+                self.status = 3
+                pygame.draw.circle(self.surf, GREEN, (self.width / 2, self.height / 2), self.radius)
